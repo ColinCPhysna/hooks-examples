4c4d476a2b9f96059e9a18707383403717ec8076
ColinCPhysna <ccasto@physna.com>
2019-10-23 11:36:50 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import useAxios from 'axios-hooks'
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+const Root = () => {
+ const [{data,loading,error}, refetch] = useAxios('https://randomuser.me/api/');
+
+ if(loading) return <p>Loading...</p>
+ if(error) return <p>Error!</p>
+  
+ let user = data.results[0]
+ let fullName = `${user.name.title}  ${user.name.first}  ${user.name.last}`
+
+ return (
+   <div>
+     <h2>{fullName}</h2>
+     <img src={user.picture.large} alt="a random fake user"/>
+     <h3><span>{user.dob.age}</span> <span>{user.location.city}</span></h3>
+     <button onClick={refetch}>Refetch</button>
+   </div>
+ )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
